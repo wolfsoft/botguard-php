7b3e294d7efde06502397f2b09dca25297da87db
Dennis Prochko <wolfsoft@mail.ru>
2018-08-22 21:54:06 +0300

    documentation added

--- example/index.php
+++ example/index.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the BotGuard PHP API Connector.
+ *
+ * (c) 2018 Dennis Prochko <wolfsoft@mail.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 use BotGuard\BotGuard;
 
 /*
